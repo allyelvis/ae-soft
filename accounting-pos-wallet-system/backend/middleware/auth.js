830e95b329440dfa95647f3717ab79550bf179c1
Nzeyimana Ally Elvis <35286164-nzeyimanaallyel@users.noreply.replit.com>
2024-09-19 22:00:21 +0000

    your commit message

--- accounting-pos-wallet-system/backend/middleware/auth.js
+++ accounting-pos-wallet-system/backend/middleware/auth.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.authMiddleware = (req, res, next) => {
   const token = req.header('Authorization');
-
+  
   if (!token) return res.status(401).json({ message: 'Access denied' });
 
   try {
